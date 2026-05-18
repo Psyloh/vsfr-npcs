79eadc45f120a707b59c57f03d26d7b76e79ec52
Psyloh <psylozoff@gmail.com>
2026-05-18 23:08:28 +0200

    Now can change config through commands

--- vsfrnpcs/src/ServerSide.cs
+++ vsfrnpcs/src/ServerSide.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.CommandAbbr;
 using Vintagestory.API.Server;
+using Vintagestory.GameContent;
 
 namespace VSFRNPCS.Server
 {
@@ -33,6 +36,11 @@ namespace VSFRNPCS.Server
 
 			return config;
 		}
+
+		public void Save()
+		{
+			ApiHelper.Api.StoreModConfig(this, FILENAME);
+		}
 	}
 
 	public class ServerSide : IDisposable
@@ -50,12 +58,49 @@ namespace VSFRNPCS.Server
 
 			_dungeonReset = api.WorldManager.SaveGame.GetData<Dictionary<string, long>>("dungeonReset") ?? [];
 
-			api.ChatCommands.Create("regenArch").RequiresPrivilege(Privilege.chat).WithDesc("Testing").HandleWith(args => {
+			api.ChatCommands.Create("resetDungeonDelay")
+				.WithAlias("rdd")
+				.RequiresPrivilege(Privilege.controlserver)
+				.WithArgs(api.ChatCommands.Parsers.Word("name"), api.ChatCommands.Parsers.OptionalDouble("delay"))
+				.HandleWith(args =>
+				{
+					var name = (string)args[0];
+					if (name != "global")
+					{
+						var structure = api.ModLoader.GetModSystem<GenStoryStructures>().Structures.Get(name);
+						if (structure == null)
+						{
+							return TextCommandResult.Success($"{name} is not a valid dungeon name");
+						}
 
-				Helpers.ResetDungeon(api, "resonancearchive");
+						if (args.Parsers[1].IsMissing)
+						{
+							return Config.Delays.TryGetValue(name, out var delay) ?
+								TextCommandResult.Success($@"""{name}"" dungeon reset delay is {delay} hours") :
+								TextCommandResult.Success($@"""{name}"" dungeon doesn't have a specified delay and thus falls back to global delay : {Config.GlobalDelay} hours");
+						}
 
-				return TextCommandResult.Success("Disappeared!");
-			});
+						var value = (double)args[1];
+						Config.Delays[name] = value;
+						Config.Save();
+
+						return TextCommandResult.Success($@"Changed ""{name}"" dungeon reset delay to {value} hours");
+					}
+					else
+					{
+						if (args.Parsers[1].IsMissing)
+						{
+							return TextCommandResult.Success($@"Global dungeon reset delay is {Config.GlobalDelay} hours");
+						}
+
+						var value = (double)args[1];
+						Config.GlobalDelay = value;
+						Config.Save();
+
+						return TextCommandResult.Success($@"Changed global dungeon reset delay to {value} hours");
+					}
+				}
+			);
 
 			api.Event.GameWorldSave += Save;
 		}
